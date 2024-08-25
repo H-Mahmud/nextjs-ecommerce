51cb6f29aa042c2585aa117d42ef494f9c22e968
Mahmud <me@imahmud.com>
2024-08-25 21:11:50 +0600

    Add customer & Product data

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -20,14 +20,55 @@ async function getSalesData() {
   };
 }
 
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { inAvailableForPurchase: true } }),
+    db.product.count({ where: { inAvailableForPurchase: false } }),
+  ]);
+
+  return { activeCount, inactiveCount };
+}
+
 export default async function AdminDashboard() {
-  const saleData = await getSalesData();
+  const [saleData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
+
   return (
     <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
       <DashboardCard
         title='Sales'
         subtitle={`${formatNumber(saleData.numberOfSales)} Orders`}
         body={formatCurrentcy(saleData.amount)}
+      />
+      <DashboardCard
+        title='Customers'
+        subtitle={`${formatCurrentcy(userData.averageValuePerUser)} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title='Active products'
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   );
