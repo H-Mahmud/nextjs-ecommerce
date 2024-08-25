ef0108073208b6046422d879d60218352d28f256
Mahmud <me@imahmud.com>
2024-08-25 20:49:15 +0600

    Build sales data card

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,53 @@
-export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import db from '@/prisma/db';
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+export default async function AdminDashboard() {
+  const saleData = await getSalesData();
+  return (
+    <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      <DashboardCard
+        title='Sales'
+        subtitle={saleData.numberOfSales}
+        body={saleData.amount}
+      />
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{subtitle}</CardDescription>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
