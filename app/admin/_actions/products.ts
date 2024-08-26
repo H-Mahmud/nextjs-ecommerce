132c57b18af6b554528eba62cb8f88f92fa787bb
Mahmud <me@imahmud.com>
2024-08-26 15:06:12 +0600

    Create product edit page

--- app/admin/_actions/products.ts
+++ app/admin/_actions/products.ts
@@ -3,7 +3,7 @@
 import db from '@/prisma/db';
 import { z } from 'zod';
 import fs from 'fs/promises';
-import { redirect } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 const fileSchema = z.instanceof(File, { message: 'Required' });
 const imageSchema = fileSchema.refine(
@@ -38,6 +38,54 @@ export async function addProduct(previousState: unknown, formData: FormData) {
   );
 
   await db.product.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath,
+      imagePath,
+    },
+  });
+
+  redirect('/admin/products');
+}
+
+const editSchema = addSchema.extend({
+  file: fileSchema.optional(),
+  image: imageSchema.optional(),
+});
+export async function updateProduct(
+  id: string,
+  previousState: unknown,
+  formData: FormData
+) {
+  const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
+  const product = await db.product.findUnique({ where: { id } });
+  if (product == null) return notFound();
+
+  let filePath = product.filePath;
+  if (data.file != null && data.file.size > 0) {
+    await fs.unlink(filePath);
+    filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+  }
+  let imagePath = product.imagePath;
+  if (data.image != null && data.image.size > 0) {
+    await fs.unlink(`public${imagePath}`);
+    imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+    await fs.writeFile(
+      `public${imagePath}`,
+      Buffer.from(await data.image.arrayBuffer())
+    );
+  }
+
+  await db.product.update({
+    where: { id },
     data: {
       name: data.name,
       description: data.description,
