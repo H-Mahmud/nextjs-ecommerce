82775c1ab47914bef03314234c271d95543d7fb6
Mahmud <me@imahmud.com>
2024-08-26 08:50:25 +0600

    Fix: Declare products action in server file

---
+++ app/admin/products/_actions/products.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import db from '@/prisma/db';
+import { notFound } from 'next/navigation';
+
+export async function togglerProductAvailability(
+  id: string,
+  isAvailableForPurchase: boolean
+) {
+  await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
+}
+
+export async function deleteProduct(id: string) {
+  const product = await db.product.delete({ where: { id } });
+  if (product == null) return notFound();
+}
--- app/admin/products/_components/productActions.tsx
+++ app/admin/products/_components/productActions.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
-import db from '@/prisma/db';
-import { notFound } from 'next/navigation';
 import { useTransition } from 'react';
+import {
+  deleteProduct,
+  togglerProductAvailability,
+} from '../_actions/products';
 
 export function ActiveTogglerDropdownItem({
   id,
@@ -46,16 +48,4 @@ export function DeleteDropdownItem({
       Delete
     </DropdownMenuItem>
   );
-}
-
-async function togglerProductAvailability(
-  id: string,
-  isAvailableForPurchase: boolean
-) {
-  await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
-}
-
-async function deleteProduct(id: string) {
-  const product = await db.product.delete({ where: { id } });
-  if (product == null) return notFound();
 }
