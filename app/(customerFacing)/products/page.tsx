d4584901350b2233241fdb5568c858343cc16fce
Mahmud <me@imahmud.com>
2024-08-28 17:59:36 +0600

    Build products page

---
+++ app/(customerFacing)/products/page.tsx
@@ -0,0 +1,36 @@
+import ProductCard, { ProductCardSkeleton } from '@/components/ProductCard';
+import db from '@/prisma/db';
+import { Suspense } from 'react';
+
+function getProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { name: 'asc' },
+  });
+}
+
+export default function ProductsPage() {
+  return (
+    <main className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsSuspense />
+      </Suspense>
+    </main>
+  );
+}
+async function ProductsSuspense() {
+  return (await getProducts()).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+}
