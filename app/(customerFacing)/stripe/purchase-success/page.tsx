bc4aef175a45727e58b39f0047b4fb8b792c1238
Mahmud <me@imahmud.com>
2024-08-29 03:41:40 +0600

    Create stripe payment success page

--- app/(customerFacing)/stripe/purchase-success/page.tsx
+++ app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,3 +1,62 @@
-export default function StripePurchasePage() {
-  return <h1>Purchase successful</h1>;
+import { Button } from '@/components/ui/button';
+import { formatCurrency } from '@/lib/formatters';
+import db from '@/prisma/db';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+export default async function StripePurchasePage({
+  searchParams,
+}: {
+  searchParams: { payment_intent: string };
+}) {
+  const paymentIntent = await stripe.paymentIntents.retrieve(
+    searchParams.payment_intent
+  );
+
+  if (paymentIntent.metadata.productId == null) return notFound();
+
+  const product = await db.product.findUnique({
+    where: { id: paymentIntent.metadata.productId },
+  });
+
+  if (product == null) notFound();
+
+  const isSuccess = paymentIntent.status == 'succeeded';
+
+  return (
+    <div className='mx-auto w-full max-w-5xl space-y-8'>
+      <h1 className={`text-4xl font-bold ${!isSuccess && 'text-destructive'}`}>
+        {isSuccess ? 'Success!' : 'Error!'}
+      </h1>
+      <div className='flex items-center gap-4'>
+        <div className='relative aspect-video w-1/3 flex-shrink-0'>
+          <Image
+            src={product.imagePath}
+            alt={product.name}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div>
+          <div className='text-lg'>
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className='text-2xl font-bold'>{product.name}</h1>
+          <div className='line-clamp-3 text-muted-foreground'>
+            {product.description}
+          </div>
+          <Button className='mt-4' size='lg' asChild>
+            {isSuccess ? (
+              <a></a>
+            ) : (
+              <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
+            )}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
